b4cba0264fae015be707accf850c3dfa5fcae45c
kashapovin <kashapovin@gmail.com>
2022-11-26 19:10:54 +0500

    added all files

---
+++ regular_expressions.py
@@ -0,0 +1,55 @@
+import csv
+import re
+
+data = []
+with open('phonebook_raw.csv') as file:
+    fields = file.readline().strip().split(',')
+    reader = csv.DictReader(file, fieldnames=fields)
+    for row in reader:
+        pattern = '([А-Яа-я]+)\s?([А-Яа-я]+)?\s{0,1}([А-Яа-я]+)?'
+
+        res = re.search(pattern, row['lastname'])
+
+        lastname, firstname, surname = res.group(1), res.group(2), res.group(3)
+
+        pattern2 = '([А-Яа-я]+)?\s?([А-Яа-я]+)?'
+        res = re.search(pattern2, row['firstname'])
+        firstname2, surname2 = res.group(1), res.group(2)
+
+        row['lastname'] = lastname
+        firstname = firstname or firstname2
+        row['firstname'] = firstname
+        surname = surname or surname2 or row['surname']
+        row['surname'] = surname
+
+        phone_pattern = r'(\+7|8)?\s?\(?(\d{3})\)?[-\s]?(\d{3})[-\s]?(\d{2})[-\s]?(\d{2})\s?\(?(доб.\s\d{4})?\)?'
+        if row['phone']:
+            res = re.sub(phone_pattern, r'+7(\2)\3-\4-\5 \6', row['phone'])
+            row['phone'] = res
+        data.append(row)
+
+data = sorted(data, key=lambda i: i['lastname'])
+data1 = []
+
+for i in range(1, len(data)):
+    if data[i-1]['lastname'] == data[i]['lastname']:
+        for key, value in data[i].items():
+            data[i][key] = value if value else data[i-1][key]
+    data1.append(data[i])
+
+# for i in range(1, len(data)):
+#     if data[i - 1]['lastname'] == data[i]['lastname']:
+#         data[i]['lastname'] = data[i - 1]['lastname'] or data[i]['lastname']
+#         data[i]['firstname'] = data[i - 1]['firstname'] or data[i]['firstname']
+#         data[i]['surname'] = data[i - 1]['surname'] or data[i]['surname']
+#         data[i]['organization'] = data[i - 1]['organization'] or data[i]['organization']
+#         data[i]['position'] = data[i - 1]['position'] or data[i]['position']
+#         data[i]['phone'] = data[i - 1]['phone'] or data[i]['phone']
+#         data[i]['email'] = data[i - 1]['email'] or data[i]['email']
+#     data1.append(data[i])
+
+print(data1)
+
+with open('phonebook_raw.csv') as file:
+    datawriter = csv.writer(file, delimiter=',')
+    datawriter.writerows(data1)
